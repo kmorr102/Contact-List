0442fc07f37acae01e2fb7b665be345814ce4ad7
Kendra Morris <kendranmorris@gmail.com>
2023-08-03 20:25:28 -0500

    table updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
-import ContactList from './components/ContactList'
+import dummyContacts from './components/ContactList'
 import './App.css'
 
 function App() {
- 
+  const[contacts,setContacts]=useState(dummyContacts);
+  console.log("contacts:",contacts);
+
+
   return (
     <>
-    
+    <table>
+      <thead>
+        <tr>
+          <th>Contact List</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Phone</td>
+        </tr>
+      </tbody>
+    </table>
+ 
     </>
   )
 }
